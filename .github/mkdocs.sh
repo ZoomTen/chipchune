87a0c2bd580ff454947f7d3610483d34ee23f8a0
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2023-03-13 21:37:22 +0700

    docs

---
+++ .github/mkdocs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -eu
+
+BUILDROOT="docbuild"
+
+pdoc --output-dir "$BUILDROOT" chipchune
+
+[ "$GH_PASSWORD" ] || exit 12
+
+git clone -b gh-pages "https://zoomten:$GH_PASSWORD@github.com/$GITHUB_REPOSITORY.git" gh-pages
+
+cp -R docbuild/* gh-pages/
+
+cd gh-pages
+
+git add *
+
+if git diff --staged --quiet; then
+  echo "$0: No changes to commit."
+  exit 0
+fi
+
+if ! git config user.name; then
+    git config user.name 'github-actions'
+    git config user.email '41898282+github-actions[bot]@users.noreply.github.com'
+fi
+
+git commit -a -m "CI: Update docs for ${GITHUB_REF#refs/tags/} ($head)"
+git push
